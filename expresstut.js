8204b9e5957b0b2e1945211ded349cc9ae3cc2b2
Jalal <jalalhaider.ideabox@gmail.com>
2017-04-10 11:28:09 +0500

    initial commit
    Express Application

---
+++ expresstut.js
@@ -0,0 +1,13 @@
+/**
+ * Created by Hp on 05-Apr-17.
+ */
+var express = require('express');
+var app = express();
+app.disable('x-powered-by');
+app.set('port', process.env.PORT || 3000);
+app.get('/', function (req,res) {
+   res.send('Express Working');
+});
+app.listen(app.get('port'), function () {
+    console.log('Express starting working press ctrl-c to stop');
+})
